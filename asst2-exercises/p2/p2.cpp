88f6aae8d826f2fe6bbc4cb7d57932328fd37628
Shlok Natarajan <shlok.natarajan@gmail.com>
2025-10-27 15:52:48 -0700

    feat: p2 done

--- asst2-exercises/p2/p2.cpp
+++ asst2-exercises/p2/p2.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <string>
 #include <mutex>
+#include <chrono>
 
 int global_counter = 0;
 
@@ -13,13 +14,23 @@ void increment_counter(int n) {
     global_counter = global_counter + n;
 }
 
+void loop_increment() {
+    std::lock_guard<std::mutex> lock(mtx);
+    for (int i=0; i<10000000; i++) {
+        global_counter += 1;
+    }
+}
+
 int main() {
-    std::thread thread1(increment_counter, 1);
-    std::thread thread2(increment_counter, 2);
+    const auto start_time = std::chrono::high_resolution_clock::now();
+    std::thread thread1(loop_increment);
+    std::thread thread2(loop_increment);
     thread1.join();
     thread2.join();
-    increment_counter(3);
     std::cout << "Current Count: " << global_counter << std::endl;
+    const auto end_time = std::chrono::high_resolution_clock::now();
+    const auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    std::cout << "Elapsed Time " << elapsed_time << std::endl;
 
     return 0;
 }
